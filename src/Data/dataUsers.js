3800132101cc2ea8587bcae04dd78f7549a4e1d1
damir <dimayu82@gmail.com>
2023-04-19 15:18:27 +0300

    Initial commit

---
+++ src/Data/dataUsers.js
@@ -0,0 +1,402 @@
+const dataUsers = [
+  {
+    "name": "Poole",
+    "surename": "Duncan"
+  },
+  {
+    "name": "Landry",
+    "surename": "Norris"
+  },
+  {
+    "name": "Sylvia",
+    "surename": "Lara"
+  },
+  {
+    "name": "Violet",
+    "surename": "Madden"
+  },
+  {
+    "name": "Cochran",
+    "surename": "Cox"
+  },
+  {
+    "name": "Raymond",
+    "surename": "Boyer"
+  },
+  {
+    "name": "Stewart",
+    "surename": "Baird"
+  },
+  {
+    "name": "Tami",
+    "surename": "Britt"
+  },
+  {
+    "name": "Henrietta",
+    "surename": "Lindsey"
+  },
+  {
+    "name": "Margret",
+    "surename": "Mueller"
+  },
+  {
+    "name": "Blankenship",
+    "surename": "Hansen"
+  },
+  {
+    "name": "Myrna",
+    "surename": "Ryan"
+  },
+  {
+    "name": "Pearl",
+    "surename": "Potter"
+  },
+  {
+    "name": "Lee",
+    "surename": "Mayer"
+  },
+  {
+    "name": "Stout",
+    "surename": "Mcpherson"
+  },
+  {
+    "name": "Gardner",
+    "surename": "Miller"
+  },
+  {
+    "name": "Kaye",
+    "surename": "Cain"
+  },
+  {
+    "name": "Peggy",
+    "surename": "French"
+  },
+  {
+    "name": "Kirby",
+    "surename": "Schneider"
+  },
+  {
+    "name": "Lara",
+    "surename": "Lowery"
+  },
+  {
+    "name": "Tara",
+    "surename": "Merritt"
+  },
+  {
+    "name": "Joanna",
+    "surename": "Blankenship"
+  },
+  {
+    "name": "Loraine",
+    "surename": "Kramer"
+  },
+  {
+    "name": "Cooper",
+    "surename": "Simon"
+  },
+  {
+    "name": "Reyna",
+    "surename": "Allen"
+  },
+  {
+    "name": "Leila",
+    "surename": "Franks"
+  },
+  {
+    "name": "Rochelle",
+    "surename": "Meyer"
+  },
+  {
+    "name": "Garner",
+    "surename": "Randolph"
+  },
+  {
+    "name": "Warner",
+    "surename": "Henry"
+  },
+  {
+    "name": "Julianne",
+    "surename": "Blake"
+  },
+  {
+    "name": "Tania",
+    "surename": "Bridges"
+  },
+  {
+    "name": "Montoya",
+    "surename": "Pugh"
+  },
+  {
+    "name": "Rodriguez",
+    "surename": "Ramirez"
+  },
+  {
+    "name": "Caitlin",
+    "surename": "Haley"
+  },
+  {
+    "name": "Ines",
+    "surename": "Howard"
+  },
+  {
+    "name": "Nunez",
+    "surename": "Chan"
+  },
+  {
+    "name": "Hatfield",
+    "surename": "Hogan"
+  },
+  {
+    "name": "Nellie",
+    "surename": "Holmes"
+  },
+  {
+    "name": "Misty",
+    "surename": "Hurley"
+  },
+  {
+    "name": "Rodgers",
+    "surename": "Lewis"
+  },
+  {
+    "name": "Claudia",
+    "surename": "Peck"
+  },
+  {
+    "name": "Pacheco",
+    "surename": "Richardson"
+  },
+  {
+    "name": "Reese",
+    "surename": "Benson"
+  },
+  {
+    "name": "Sofia",
+    "surename": "Dawson"
+  },
+  {
+    "name": "Estela",
+    "surename": "Allison"
+  },
+  {
+    "name": "Ida",
+    "surename": "Mann"
+  },
+  {
+    "name": "Bradshaw",
+    "surename": "Dominguez"
+  },
+  {
+    "name": "Carole",
+    "surename": "Mcmahon"
+  },
+  {
+    "name": "Ramona",
+    "surename": "Bray"
+  },
+  {
+    "name": "Luann",
+    "surename": "Salinas"
+  },
+  {
+    "name": "Liliana",
+    "surename": "Garrett"
+  },
+  {
+    "name": "Fisher",
+    "surename": "Alvarado"
+  },
+  {
+    "name": "Dorothy",
+    "surename": "Pate"
+  },
+  {
+    "name": "Sharp",
+    "surename": "Baxter"
+  },
+  {
+    "name": "Graham",
+    "surename": "Ford"
+  },
+  {
+    "name": "Caroline",
+    "surename": "Watkins"
+  },
+  {
+    "name": "Cathryn",
+    "surename": "Forbes"
+  },
+  {
+    "name": "Marcella",
+    "surename": "Reynolds"
+  },
+  {
+    "name": "Mccormick",
+    "surename": "Hayes"
+  },
+  {
+    "name": "Pearlie",
+    "surename": "Donovan"
+  },
+  {
+    "name": "Gwen",
+    "surename": "Hardin"
+  },
+  {
+    "name": "Mavis",
+    "surename": "Obrien"
+  },
+  {
+    "name": "Joanne",
+    "surename": "Cohen"
+  },
+  {
+    "name": "Janette",
+    "surename": "Delacruz"
+  },
+  {
+    "name": "Maryann",
+    "surename": "Osborne"
+  },
+  {
+    "name": "Cathleen",
+    "surename": "Mcclure"
+  },
+  {
+    "name": "Maribel",
+    "surename": "Valentine"
+  },
+  {
+    "name": "Olivia",
+    "surename": "Phelps"
+  },
+  {
+    "name": "Allie",
+    "surename": "Farley"
+  },
+  {
+    "name": "Hannah",
+    "surename": "Sullivan"
+  },
+  {
+    "name": "Alta",
+    "surename": "Acosta"
+  },
+  {
+    "name": "Fanny",
+    "surename": "Price"
+  },
+  {
+    "name": "Faye",
+    "surename": "Charles"
+  },
+  {
+    "name": "Leonor",
+    "surename": "Cochran"
+  },
+  {
+    "name": "Alyce",
+    "surename": "Wilkins"
+  },
+  {
+    "name": "Mejia",
+    "surename": "Jarvis"
+  },
+  {
+    "name": "Hopper",
+    "surename": "Rutledge"
+  },
+  {
+    "name": "Evans",
+    "surename": "Herring"
+  },
+  {
+    "name": "Terri",
+    "surename": "Watts"
+  },
+  {
+    "name": "Mitchell",
+    "surename": "Landry"
+  },
+  {
+    "name": "Nadine",
+    "surename": "Walter"
+  },
+  {
+    "name": "Frederick",
+    "surename": "Poole"
+  },
+  {
+    "name": "Dale",
+    "surename": "Hudson"
+  },
+  {
+    "name": "Hansen",
+    "surename": "Santiago"
+  },
+  {
+    "name": "Melisa",
+    "surename": "Hanson"
+  },
+  {
+    "name": "Margaret",
+    "surename": "Acevedo"
+  },
+  {
+    "name": "Cabrera",
+    "surename": "Austin"
+  },
+  {
+    "name": "Billie",
+    "surename": "Byrd"
+  },
+  {
+    "name": "Floyd",
+    "surename": "Ellis"
+  },
+  {
+    "name": "Patricia",
+    "surename": "Gates"
+  },
+  {
+    "name": "Bette",
+    "surename": "Beasley"
+  },
+  {
+    "name": "Mathis",
+    "surename": "Cash"
+  },
+  {
+    "name": "Branch",
+    "surename": "Stephenson"
+  },
+  {
+    "name": "Ana",
+    "surename": "Everett"
+  },
+  {
+    "name": "Carly",
+    "surename": "Moss"
+  },
+  {
+    "name": "Callahan",
+    "surename": "Weeks"
+  },
+  {
+    "name": "Ingram",
+    "surename": "Kemp"
+  },
+  {
+    "name": "Anderson",
+    "surename": "Marquez"
+  },
+  {
+    "name": "Eloise",
+    "surename": "Simmons"
+  },
+  {
+    "name": "Ivy",
+    "surename": "Mathis"
+  }
+]
