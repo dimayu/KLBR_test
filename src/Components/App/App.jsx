0d69b9decedffae4077a654010f1b0d015794c32
damir <dimayu82@gmail.com>
2023-04-20 14:17:08 +0300

    Scroll appearance and animation

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,14 +1,37 @@
+import { useEffect, useState } from 'react';
+
 import { dataUsers } from '../../Data/dataUsers';
 import { User } from '../index';
 
 import './App.scss';
 
 export const App = () => {
+  const [count, setCount] = useState(20);
+  let data = dataUsers.slice(0, count);
+  const flag = Boolean(count >= dataUsers.length);
+  
+  useEffect(() => {
+    document.addEventListener('scroll', scrollHandler);
+    return function () {
+      document.removeEventListener('scroll', scrollHandler);
+    };
+  }, []);
+  
+  useEffect(() => {
+    data = data.slice(0, count);
+  }, [count]);
+  
+  const scrollHandler = (e) => {
+    if (e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight) < 100 && !flag) {
+      setCount(prevCount => prevCount + 20);
+    }
+  };
+  
   return (
     <div className="App">
-      {dataUsers.map((item) =>
-        <User item={item} key={item.id}/>
+      {data.map((item) =>
+        <User item={item} key={item.id} />
       )}
     </div>
   );
-}
+};
--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,8 +1,16 @@
+import { useEffect } from 'react';
+import WOW from 'wowjs';
+
 import './User.scss';
 
 export const User = ({ item }) => {
+  useEffect(() => {
+    const wow = new WOW.WOW({live: false});
+    wow.init()
+  }, []);
+  
   return (
-    <div className="user">
+    <div className="user wow fadeInUp" data-wow-iteration="1">
       <h3 className="user__name">{item.name}</h3>
       <h4 className="user__surname">{item.surename}</h4>
     </div>
